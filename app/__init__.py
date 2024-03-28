8fc81f1751104969639ce0b0c7f7b2dc1e75bafe
Kiran-Ramisetti-kr483 <kr483@njit.edu>
2024-03-28 14:57:35 -0400

    updated plugins in app code

--- app/__init__.py
+++ app/__init__.py
@@ -21,17 +21,25 @@ class App:
     
     def load_plugins(self):
         # Dynamically load all plugins in the plugins directory
-        plugins_package = 'app.plugins'
-        for _, plugin_name, is_pkg in pkgutil.iter_modules([plugins_package.replace('.', '/')]):
-            if is_pkg:  # Ensure it's a package
-                plugin_module = importlib.import_module(f'{plugins_package}.{plugin_name}')
-                for item_name in dir(plugin_module):
-                    item = getattr(plugin_module, item_name)
-                    try:
-                        if issubclass(item, (Command)):  # Assuming a BaseCommand class exists
-                            self.command_handler.register_command(plugin_name, item())
-                    except TypeError:
-                        continue  # If item is not a class or unrelated class, just ignore
+        plugins_packages = [
+            'app.plugins.addition',
+            'app.plugins.subtraction',
+            'app.plugins.multiplication',
+            'app.plugins.division',
+            'app.plugins.menu'
+        ]
+        for plugins_package in plugins_packages:
+            for _, plugin_name, is_pkg in pkgutil.iter_modules([plugins_package.replace('.', '/')]):
+                if is_pkg:  # Ensure it's a package
+                    plugin_module = importlib.import_module(f'{plugins_package}.{plugin_name}')
+                    for item_name in dir(plugin_module):
+                        item = getattr(plugin_module, item_name)
+                        try:
+                            if issubclass(item, (Command)):  # Assuming a BaseCommand class exists
+                                self.command_handler.register_command(plugin_name, item())
+                        except TypeError:
+                            continue  # If item is not a class or unrelated class, just ignore
+
     def start(self):
         # Register commands here
         self.load_plugins()
